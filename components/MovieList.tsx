a04d0e686920a6149ff48aeb6d201a5b46f8ec31
APPLE <apple@APPLEs-MacBook-Pro.local>
2023-11-16 11:23:41 -0600

    small fix

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -7,7 +7,9 @@ export default function MovieList({ movies }: { movies: Movie[] }) {
     <div className="container mx-auto p-4">
       <div className="grid justify-between grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-w-7xl mx-auto">
         {movies.map((movie) => (
-          <MovieCard movie={movie}></MovieCard>
+          <div key={movie.id}>
+            <MovieCard movie={movie}></MovieCard>
+          </div>
         ))}
       </div>
     </div>
