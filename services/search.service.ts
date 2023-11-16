e36f8bb49bf25d400ff1ab86b0c471571fb084a9
APPLE <apple@APPLEs-MacBook-Pro.local>
2023-11-16 11:08:35 -0600

    replaced api token and toke with env

--- services/search.service.ts
+++ services/search.service.ts
@@ -6,7 +6,7 @@ export default async function searchMovies(query_string: string) {
     headers: {
       accept: "application/json",
       Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZDI5YTUyMDZiZjk4YmZmZDkxMDZmYzE2ZTUxYTFhZSIsInN1YiI6IjY1NTA5MDI5ZDRmZTA0MDEzOTdmYjc2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Zd9bmo-i11f0QkhsoZtxHdkAdw0cuiU13scU9scWbQk",
+        "Bearer "+process.env.token,
     },
   };
 
