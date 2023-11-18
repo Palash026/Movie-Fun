948e3e5b383cf5754d886644731dd59573fc20e2
APPLE <apple@APPLEs-MacBook-Pro.local>
2023-11-18 00:06:24 -0600

    Testing deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-import getMovie from "@/services/movie.service";
-import Movie from "@/interface/movie.interface";
-import MovieList from "@/components/MovieList";
-
-export default async function Home() {
-  const res = await (await getMovie()).json();
-  const movies: Movie[] = res?.results;
-
-  return <MovieList movies={movies}/>
+export default function Home() {
+  return (
+    <>
+    </>
+  )
 }
