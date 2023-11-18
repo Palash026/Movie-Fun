083dc4f09c50913f9946e7f4d5d44a8da21b08ab
APPLE <apple@APPLEs-MacBook-Pro.local>
2023-11-18 04:09:03 -0600

    reset developed

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,24 +1,22 @@
 "use client"
-// import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-
+import { useRouter } from 'next/navigation';
 export default function Search() {
-    const [query, setQuery] = useState('');
-    // const router = useRouter();
-
-    // useEffect(() => {
-    //     if(query){
-    //         router.push('/movies/search?query='+query);
-    //     }else{
-    //         router.push('/');
-    //     }
-    // }, [query])
+  const router = useRouter();
+ 
+  function handleSearch(term: string) {
+    
+    if (term) {
+      router.push('/movies/search?query='+term);
+    } else {
+      router.push('/')
+    }
+  }
   return (
     <input
       type="text"
       placeholder="Search..."
       className="px-4 py-2 rounded-lg bg-gray-200 text-blue-600 focus:outline-none w-full md:max-w-md"
-      onChange={(e) => setQuery(e.target.value)}
+      onChange={(e) => handleSearch(e.target.value)}
     />
   );
 }
