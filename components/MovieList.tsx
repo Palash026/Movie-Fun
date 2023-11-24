38d14b46c570b0a517f578d95f3284cf74435069
APPLE <apple@APPLEs-MacBook-Pro.local>
2023-11-24 14:06:13 -0600

    cleaning code

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -5,13 +5,19 @@ import MovieCard from "./MovieCard";
 export default function MovieList({ movies }: { movies: Movie[] }) {
   return (
     <div className="container mx-auto p-4">
-      <div className="grid justify-between grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-w-7xl mx-auto">
-        {movies.map((movie) => (
-          <div key={movie?.id}>
-            <MovieCard movie={movie}></MovieCard>
-          </div>
-        ))}
-      </div>
+      {movies.length ? (
+        <div className="grid justify-between grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-w-7xl mx-auto">
+          {movies.map((movie) => (
+            <div key={movie?.id}>
+              <MovieCard movie={movie}></MovieCard>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="h-96 flex items-center justify-center">
+          <p className="text-center">Movies not available</p>
+        </div>
+      )}
     </div>
   );
 }
--- interface/movie.interface.ts
+++ interface/movie.interface.ts
@@ -1,8 +1,20 @@
 export default interface Movie {
-    id: string;
-    name: string;
-    title: string;
-    releaseDate: Date;
-    image: string;
-    poster_path: string;
+  adult: boolean;
+  backdrop_path: string;
+  id: number;
+  name: string;
+  original_language: string;
+  original_name: string;
+  overview: string;
+  poster_path: string;
+  media_type: string;
+  genre_ids: number[];
+  popularity: number;
+  first_air_date: string;
+  vote_average: number;
+  vote_count: number;
+  origin_country: string[];
+  title: string;
+  release_date: string;
+  runtime: number;
 }
